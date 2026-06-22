41480dd21e9b1821fde311b22e9e62e1c49b4e71
Fahim <fahimyadgir07@gmail.com>
2026-06-22 15:37:01 +0530

    more

--- perform query.sql
+++ perform query.sql
@@ -342,4 +342,5 @@ group by product_name , quantity ;
 select c.customer_id,c.customer_name,o.product_name,quantity,
 		sum(o.price * o.quantity) over (order by order_id)as total_revenue
 from customers c
-left join orders o on c.customer_id = o.customer_id;
+left join orders o on c.customer_id = o.customer_id
+where customer_city = 'Pune';
